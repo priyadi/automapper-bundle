0008843d18ba3f527640b3ffccd8b869e2cb91c7
Priyadi Iman Nurcahyo <priyadi@priyadi.net>
2023-12-21 23:03:26 +0700

    Wrap TypeExtractors in a decorator

--- src/DependencyInjection/Compiler/PropertyInfoPass.php
+++ src/DependencyInjection/Compiler/PropertyInfoPass.php
@@ -3,6 +3,7 @@
 namespace AutoMapper\Bundle\DependencyInjection\Compiler;
 
 use AutoMapper\Extractor\MapToContextPropertyInfoExtractorDecorator;
+use AutoMapper\Extractor\PropertyTypeExtractorDecorator;
 use Symfony\Component\DependencyInjection\Argument\IteratorArgument;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
@@ -41,6 +42,17 @@ class PropertyInfoPass implements CompilerPassInterface
         );
 
         $container->setDefinition(
+            'automapper.property_info.property_type_extractor',
+            new Definition(
+                PropertyTypeExtractorDecorator::class,
+                [
+                    new Reference('property_info.php_doc_extractor'),
+                    new Reference('property_info.reflection_extractor'),
+                ]
+            )
+        );
+
+        $container->setDefinition(
             'automapper.property_info',
             new Definition(
                 PropertyInfoExtractor::class,
@@ -49,8 +61,7 @@ class PropertyInfoPass implements CompilerPassInterface
                         new Reference('automapper.property_info.reflection_extractor'),
                     ]),
                     new IteratorArgument([
-                        new Reference('automapper.property_info.reflection_extractor'),
-                        new Reference('property_info.php_doc_extractor'),
+                        new Reference('automapper.property_info.property_type_extractor'),
                     ]),
                     new IteratorArgument([
                         new Reference('automapper.property_info.reflection_extractor'),
